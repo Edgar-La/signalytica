11693e514713565fe0d3379f59606cc93daf4866
Edgar-La <laraae2018@licifug.ugto.mx>
2024-09-04 12:59:55 -0600

    changes in setup

--- setup.py
+++ setup.py
@@ -1,28 +1,55 @@
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup
 
+    _has_setuptools = True
+except ImportError:
+    from distutils.core import setup
+
+
+def check_dependencies():
+    install_requires = []
+
+    try:
+        import numpy
+    except ImportError:
+        install_requires.append("numpy")
+    try:
+        import scipy
+    except ImportError:
+        install_requires.append("scipy")
+
+    return install_requires
+
+
+# Function to parse requirements.txt
 def parse_requirements(filename):
-    """Load requirements from a pip requirements file."""
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-        return [line.strip() for line in lines if line and not line.startswith('#')]
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        # Filter out comments and empty lines
+        return [line.strip() for line in lines if line and not line.startswith("#")]
 
-setup(
-    name="signalytica",
-    version="0.1.11",
-    author="Edgar Lara",
-    author_email="elara480@gmil.com",
-    description="Feature extraction for time series and EEG signals",
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/Edgar-La/signalytica",
-    #packages=find_packages(),
-    package_dir={"": "src"},  # Tells setuptools that packages are inside the src/ directory
-    packages=find_packages(where="src"),  # Automatically find all packages inside src/
-    install_requires=parse_requirements('requirements.txt'),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',	
-)
+if __name__ == "__main__":
+    install_requires = check_dependencies()
+
+    setup(
+        name="signalytica",
+        version="0.1.16",
+        author="Edgar Lara",
+        author_email="elara480@gmail.com",
+        description="Feature extraction for time series and EEG signals",
+        long_description=open('README.md').read(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/Edgar-La/signalytica",
+        #packages=find_packages(),
+        #package_dir={"": "src"},  # Tells setuptools that packages are inside the src/ directory
+        #packages=find_packages(where="src"),  # Automatically find all packages inside src/
+        packages=["signalytica"],
+        install_requires=parse_requirements('requirements.txt'),
+        #install_requires=install_requires,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.7',
+    )
